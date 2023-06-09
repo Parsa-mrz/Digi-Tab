2fe4a0147551cfcf67b92ff986a9be4a91d0c333
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-06-09 19:38:01 +0330

    add view-order functions

--- App/Order/Orders.php
+++ App/Order/Orders.php
@@ -5,10 +5,11 @@ class Orders
 
     public function __construct()
     {
-        add_filter('wc_get_template', [$this, 'load_view'], 10, 5);
+        add_filter('wc_get_template', [$this, 'load_orders_view'], 10, 5);
+        add_filter('wc_get_template', [$this, 'load_view_orders_views'], 10, 5);
     }
 
-    public function load_view($located, $template_name, $args, $template_path, $default_path)
+    public function load_orders_view($located, $template_name, $args, $template_path, $default_path)
     {
         // Check if we're on the my-account/orders/ page
         if ($template_name === 'myaccount/orders.php') {
@@ -17,5 +18,16 @@ class Orders
 
         return $located;
     }
+
+    public function load_view_orders_views($located, $template_name, $args, $template_path, $default_path)
+    {
+        // Check if we're on the my-account/orders/ page
+        if ($template_name === 'myaccount/view-order.php') {
+            $located = ORD_LI_DIR . 'src/templates/view-order.php';
+        }
+
+        return $located;
+    }
+
 }
 new Orders();
---
+++ src/templates/view-order.php
@@ -0,0 +1,1 @@
+<h1>View orders</h1>
