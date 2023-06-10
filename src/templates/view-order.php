006f356ee36c34f1e87271b6f29f16f4acf64089
Masihbalali <Masihbalali@outlook.com>
2023-06-10 15:25:25 +0330

    VOD template

--- src/templates/view-order.php
+++ src/templates/view-order.php
@@ -50,23 +50,23 @@ foreach ($order_items as $item) {
         $download_expiry = $download['download_expiry'];
     
         // Output the download information
-        echo "Download URL: " . $download_url;
-        echo "<br>";
-        echo $download_name;
-        echo "<br>";
-        echo "Download Limit: " . $download_limit;
-        echo "<br>";
-        echo "Download Expiration: " . $download_expiry;
-        echo "<br>";
+        // echo "Download URL: " . $download_url;
+        // echo "<br>";
+        // echo $download_name;
+        // echo "<br>";
+        // echo "Download Limit: " . $download_limit;
+        // echo "<br>";
+        // echo "Download Expiration: " . $download_expiry;
+        // echo "<br>";
     }
 
-    echo $product_image;
-    echo "<br>";
-    echo 'قیمت محصول :' . $product_price;
-    echo "عنوان محصول : " . $product_title;
-    echo "<br>";
-    echo " پیوند یکتا محصول :  <a href='$product_url'>مشاهده محصول</a>";
-    echo "<br>";
+    // echo $product_image;
+    // echo "<br>";
+    // echo 'قیمت محصول :' . $product_price;
+    // echo "عنوان محصول : " . $product_title;
+    // echo "<br>";
+    // echo " پیوند یکتا محصول :  <a href='$product_url'>مشاهده محصول</a>";
+    // echo "<br>";
 }
 // order date 
 $order_date = $order->get_date_created()->format('Y-m-d');
@@ -76,33 +76,33 @@ $payment_date = $order->get_date_paid()->format('Y-m-d');
 $product_count = $order->get_item_count();
 echo "<br>";
 // echo " عکس محصول : " . $product_image;
-// echo "عنوان محصول : " . $product_title;
-echo "<br>";
-echo "تخفیف :" . $discountTotal;
-echo '<br>';
-echo "شماره سفارش :" . $order_id;
-echo '<br>';
-echo " هزینه حمل و نقل :" . $shippingTotal;
-echo '<br>';
-echo " مبلغ سفارش :" . $total;
-echo '<br>';
-echo "  تحویل گیرنده : " . $firstName . ' ' . $lastName;
-echo '<br>';
-echo "آدرس : " . $address;
-echo "<br>";
-echo "کد پستی : " . $postcode;
-echo "<br>";
-echo " شماره تماس : " . $phone;
-echo "<br>";
-echo "  روش پرداخت : " . $payment;
-echo "<br>";
-echo "تاریخ ثبت سفارش : " . $order_date;
-echo "<br>";
-echo "تاریخ  پرداخت : " . $payment_date;
-echo "<br>";
-echo "  مبلغ پرداخت شده : " . $paymentTotal;
-echo "<br>";
-echo "تعداد ایتم های سفارش" . $product_count;
+// // echo "عنوان محصول : " . $product_title;
+// echo "<br>";
+// echo "تخفیف :" . $discountTotal;
+// echo '<br>';
+// echo "شماره سفارش :" . $order_id;
+// echo '<br>';
+// echo " هزینه حمل و نقل :" . $shippingTotal;
+// echo '<br>';
+// echo " مبلغ سفارش :" . $total;
+// echo '<br>';
+// echo "  تحویل گیرنده : " . $firstName . ' ' . $lastName;
+// echo '<br>';
+// echo "آدرس : " . $address;
+// echo "<br>";
+// echo "کد پستی : " . $postcode;
+// echo "<br>";
+// echo " شماره تماس : " . $phone;
+// echo "<br>";
+// echo "  روش پرداخت : " . $payment;
+// echo "<br>";
+// echo "تاریخ ثبت سفارش : " . $order_date;
+// echo "<br>";
+// echo "تاریخ  پرداخت : " . $payment_date;
+// echo "<br>";
+// echo "  مبلغ پرداخت شده : " . $paymentTotal;
+// echo "<br>";
+// echo "تعداد ایتم های سفارش" . $product_count;
 
 
 
@@ -122,3 +122,45 @@ echo "تعداد ایتم های سفارش" . $product_count;
 // } else {
 //     echo '<p>No order ID specified.</p>';
 // }
+?>
+<div class="card-body mr-custom main-body-container">
+          <!-- <div style="display:flex;">
+            <span class="glyphicon glyphicon-ok-circle" style="font-size: 20px;margin-top: 20px;color: green;"></span>
+            <h3 class="card-title" style="padding-right:10px">تحویل شده </h3>
+          </div> -->
+          <div class="details-container-custom" style="width: 500px;">
+            <p class="card-title"> کد پیگیری سفارش</p>
+            <p class="card-title"> تاریخ ثبت سفارش </p>
+          </div>
+          <hr>
+          <div class="details-container-custom" style="width: 400px;">
+            <p class="card-title"> تحویل گیرنده</p>
+            <p class="card-title"> شماره موبایل </p>
+          </div>
+          <div class="details-container-custom">
+            <p class="card-title"> آدرس </p>
+          </div>
+          <hr>
+          <div class="details-container-custom">
+            <p class="card-title"> مبلغ </p>
+            <p class="card-title">تخفیف: </p>
+            <p class="card-title"> شیوه پرداخت: </p>
+          </div>
+          <div class="details-container-custom">
+
+            <p class="card-title"> مبلغ سفارش - پرداخت موفق </p>
+            <p class="card-title"> ۲۱۸۳۷۶﷼ </p>
+            <p class="card-title"> ۱۲/۸/۱۴۰۱ </p>
+          </div>
+
+
+          <!-- <div style="background-color: antiquewhite; border: 1px solid #ccc; border-radius: 5px; ">
+            <div class="details-container-custom">
+              <p class="card-title"> آدرس </p>
+            </div>
+          </div> -->
+          <!-- <div style="display:flex;justify-content: flex-end; color:blue;">
+            <a href="#" style="cursor:pointer; text-decoration:none;">مشاهده فاکتور</a>
+          </div> -->
+
+        </div>
