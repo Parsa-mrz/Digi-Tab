3bd8bc7150cf97a303a4877322a225109dcc6878
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-06-10 15:23:23 +0330

    add view order function

--- App/Order/Orders.php
+++ App/Order/Orders.php
@@ -20,9 +20,10 @@ class Orders
     }
 
     public function load_view_orders_views($located, $template_name, $args, $template_path, $default_path)
-    {        
+    {
         // Check if we're on the my-account/orders/ page
         if ($template_name === 'myaccount/view-order.php') {
+
             $located = ORD_LI_DIR . 'src/templates/view-order.php';
         }
 
--- src/templates/view-order.php
+++ src/templates/view-order.php
@@ -1,82 +1,11 @@
 <?php
-// order id 
-$order_id = isset($_SERVER['REQUEST_URI']) ? basename($_SERVER['REQUEST_URI']) : '';
-$order_id = preg_replace('/[^0-9]/', '', $order_id);
-// total discount 
-$discountTotal = wc_price($order->get_data()['discount_total']);
-// total shipping 
-$shippingTotal = wc_price($order->get_data()['shipping_total']);
-// total cost 
-$total = wc_price($order->get_data()['discount_total'] + $order->get_data()['total']);
-// payment total
-$paymentTotal = wc_price($order->get_data()['total']);
-// first name
-$firstName = $order->get_data()['billing']['first_name'];
-// last name
-$lastName = $order->get_data()['billing']['last_name'];
-// address
-$address = $order->get_data()['billing']['address_1'];
-// postcode 
-$postcode = $order->get_data()['billing']['postcode'];
-// phone number
-$phone = $order->get_data()['billing']['phone'];
-// payment method 
-$payment = $order->get_data()['payment_method_title'];
-// product img and title 
-$order_items = $order->get_items();
-foreach ($order_items as $item) {
-    // list of product in order 
-    $product = $item->get_product();
-    // product price 
-    $product_price = $product->get_price();
-    // product title 
-    $product_title = $product->get_title();
-    // product image 
-    $product_image = $product->get_image();
-    // product url 
-    $slug = $product->get_slug();
-    $product_url = home_url() . '/products/' . $slug;
-    // downloadable file 
-    $downloads = $product->get_downloads();
-    foreach ($downloads as $download) {
-        // Get the download URL
-        $download_url = $download['file'];
-        // name of downlaod url 
-        $download_name = $download['name'];
-        // Get the download limit (-1 indicates no limit)
-        $download_limit = $download['download_limit'];
-    
-        // Get the download expiration (-1 indicates no expiration)
-        $download_expiry = $download['download_expiry'];
-    
-        // Output the download information
-        echo "Download URL: " . $download_url;
-        echo "<br>";
-        echo $download_name;
-        echo "<br>";
-        echo "Download Limit: " . $download_limit;
-        echo "<br>";
-        echo "Download Expiration: " . $download_expiry;
-        echo "<br>";
-    }
-
-    echo $product_image;
-    echo "<br>";
-    echo 'قیمت محصول :' . $product_price;
-    echo "عنوان محصول : " . $product_title;
-    echo "<br>";
-    echo " پیوند یکتا محصول :  <a href='$product_url'>مشاهده محصول</a>";
-    echo "<br>";
-}
-// order date 
-$order_date = $order->get_date_created()->format('Y-m-d');
-// payment date 
-$payment_date = $order->get_date_paid()->format('Y-m-d');
-// number of each item 
-$product_count = $order->get_item_count();
+require_once(ORD_LI_DIR . 'App/Order/View-orders.php');
+echo $product_image;
 echo "<br>";
-// echo " عکس محصول : " . $product_image;
-// echo "عنوان محصول : " . $product_title;
+echo 'قیمت محصول :' . $product_price;
+echo "عنوان محصول : " . $product_title;
+echo "<br>";
+echo " پیوند یکتا محصول :  <a href='$product_url'>مشاهده محصول</a>";
 echo "<br>";
 echo "تخفیف :" . $discountTotal;
 echo '<br>';
@@ -103,22 +32,4 @@ echo "<br>";
 echo "  مبلغ پرداخت شده : " . $paymentTotal;
 echo "<br>";
 echo "تعداد ایتم های سفارش" . $product_count;
-
-
-
-// if ($order_id > 0) {
-//     $order = wc_get_order($order_id);
-
-//     if ($order) {
-//         // Display order details here
-//         echo '<h2>Order Details</h2>';
-//         echo '<p>Order ID: ' . $order_id . '</p>';
-//         echo '<p>Order Total: ' . $order->get_formatted_order_total() . '</p>';
-//         // You can retrieve and display any other order details you need
-
-//     } else {
-//         echo '<p>Invalid order ID.</p>';
-//     }
-// } else {
-//     echo '<p>No order ID specified.</p>';
-// }
+?>
