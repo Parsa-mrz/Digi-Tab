6ec7634a8d117eef3589439ddde34e5dfd2e29c0
Masihbalali <Masihbalali@outlook.com>
2023-06-10 16:07:02 +0330

    merge both masih and parsa changes

--- src/templates/view-order.php
+++ src/templates/view-order.php
@@ -48,3 +48,43 @@ echo "تعداد ایتم های سفارش" . $product_count;
 // } else {
 //     echo '<p>No order ID specified.</p>';
 // }
+?>
+<div class="card-body mr-custom main-body-container">
+          <!-- <div style="display:flex;">
+            <span class="glyphicon glyphicon-ok-circle" style="font-size: 20px;margin-top: 20px;color: green;"></span>
+            <h3 class="card-title" style="padding-right:10px">تحویل شده </h3>
+          </div> -->
+          <div class="details-container-custom" style="width: 500px;">
+            <p class="card-title"> کد پیگیری سفارش</p>
+            <p class="card-title"> تاریخ ثبت سفارش </p>
+          </div>
+          <hr>
+          <div class="details-container-custom" style="width: 400px;">
+            <p class="card-title"> تحویل گیرنده</p>
+            <p class="card-title"> شماره موبایل </p>
+          </div>
+          <div class="details-container-custom">
+            <p class="card-title"> آدرس </p>
+          </div>
+          <hr>
+          <div class="details-container-custom">
+            <p class="card-title"> مبلغ </p>
+            <p class="card-title">تخفیف: </p>
+            <p class="card-title"> شیوه پرداخت: </p>
+          </div>
+          <div class="details-container-custom">
+
+            <p class="card-title"> مبلغ سفارش - پرداخت موفق </p>
+            <p class="card-title"> ۲۱۸۳۷۶﷼ </p>
+            <p class="card-title"> ۱۲/۸/۱۴۰۱ </p>
+          </div>
+
+
+          <!-- <div style="background-color: antiquewhite; border: 1px solid #ccc; border-radius: 5px; ">
+            <div class="details-container-custom">
+              <p class="card-title"> آدرس </p>
+            </div>
+          </div> -->
+          <!-- <div style="display:flex;justify-content: flex-end; color:blue;">
+            <a href="#" style="cursor:pointer; text-decoration:none;">مشاهده فاکتور</a>
+          </div> -->
