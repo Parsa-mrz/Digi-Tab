aed7ab02d4f7304117752fa0b27067b8d3b9cf25
Masihbalali <Masihbalali@outlook.com>
2023-06-11 13:45:09 +0330

    VOD template done

--- src/templates/view-order.php
+++ src/templates/view-order.php
@@ -1,11 +1,7 @@
 <?php
 require_once (ORD_LI_DIR . "App/Order/View-orders.php");
 ?>
-<div class="card-body mr-custom main-body-container">
-          <!-- <div style="display:flex;">
-            <span class="glyphicon glyphicon-ok-circle" style="font-size: 20px;margin-top: 20px;color: green;"></span>
-            <h3 class="card-title" style="padding-right:10px">تحویل شده </h3>
-          </div> -->
+  <div class="card-body mr-custom main-body-container">
           <div class="details-container-custom" style="width: 500px;">
             <p class="card-title"> کد پیگیری سفارش</p>
             <p class="card-title"> تاریخ ثبت سفارش </p>
@@ -19,24 +15,44 @@ require_once (ORD_LI_DIR . "App/Order/View-orders.php");
             <p class="card-title"> آدرس </p>
           </div>
           <hr>
+
           <div class="details-container-custom">
             <p class="card-title"> مبلغ </p>
             <p class="card-title">تخفیف: </p>
             <p class="card-title"> شیوه پرداخت: </p>
+            <p class="card-title more-custom"> جزيیات </p>
           </div>
-          <div class="details-container-custom">
 
+          <div class="details-container-custom">
             <p class="card-title"> مبلغ سفارش - پرداخت موفق </p>
             <p class="card-title"> ۲۱۸۳۷۶﷼ </p>
             <p class="card-title"> ۱۲/۸/۱۴۰۱ </p>
           </div>
-
-
-          <!-- <div style="background-color: antiquewhite; border: 1px solid #ccc; border-radius: 5px; ">
-            <div class="details-container-custom">
-              <p class="card-title"> آدرس </p>
+          <hr>
+          <div class="card-body mr-custom card-details-container">
+              <div class="details-container-custom">
+                <p class="card-title"> مرسوله ۱ از ۱</p>
+                <p class="card-title" style="color:red;"> ارسال عای <i class="fa fa-truck"></i></p>
+              </div>
+              <div>
+                <p class="card-title"> زمان تحویل </p>
+              </div>
+              <div class="details-container-custom">
+                <p class="card-title"> هزینه ارسال</p>
+                <p class="card-title"> مبلغ مرسوله</p>
+                <p class="card-title"> کد پیگیری مرسوله</p>
+              </div>
+              <hr>
+              <div class="details-container-custom" style="justify-content:flex-start;">
+                <img src="./airpod.jpg" class="card-img-right card-img-custom" alt="Loading...">
+                <div class="card-order-details">
+                  <span>نام محصول اینجا قرار خواهد گرفت</span>
+                <p class="card-title"> هزینه ارسال</p>
+                <p class="card-title"> مبلغ مرسوله</p>
+                <p class="card-title"> کد پیگیری مرسوله</p>
+              </div>
+              </div>
             </div>
-          </div> -->
-          <!-- <div style="display:flex;justify-content: flex-end; color:blue;">
-            <a href="#" style="cursor:pointer; text-decoration:none;">مشاهده فاکتور</a>
-          </div> -->
+         
+
+  </div>
